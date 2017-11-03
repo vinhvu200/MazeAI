2df7899719e48a9ffb61f25a2e1be6d74dcce019
vinhvu200 <vinhvu200@gmail.com>
2017-11-03 14:24:30 -0700

    Changed value_board gridlayout's children to Buttons instead of Labels because it is more flexible for our purpose

--- Model/BorderLabel.py
+++ Model/BorderLabel.py
@@ -6,5 +6,5 @@ class BorderLabel(Label):
     def on_size(self, *args):
         self.canvas.before.clear()
         with self.canvas.before:
-            Color(0, 1, 0, 0.25)
+            Color(1, 1, 1, 0.25)
             Rectangle(pos=self.pos, size=self.size)
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.graphics import Color, Rectangle
+from kivy.clock import mainthread
 
 from Model.BorderLabel import BorderLabel
+from Model.Direction import Direction
 
 from kivy.config import Config
 Config.set('graphics', 'width', '1250')
@@ -25,8 +29,8 @@ class RootWidgit(FloatLayout):
         self.mat_walls, self.ROWS, self.COLS = self._build_matrix_walls(self.maze1)
 
         # Readjust rows and columns to fit walls in between for the board rows
-        self.BOARD_ROWS = self.ROWS * 2 - 1
-        self.BOARD_COLS = self.COLS * 2 - 1
+        self.BOARD_ROWS = self.ROWS * 2 + 1
+        self.BOARD_COLS = self.COLS * 2 + 1
 
         # Initialize the 2D maze matrix where 1 will indicate where the user is
         self.maze_board_mat = [[0 for _ in xrange(self.COLS)] for _ in xrange(self.ROWS)]
@@ -35,7 +39,11 @@ class RootWidgit(FloatLayout):
         # Initialize the 2D value matrix to perform reinforcement learning on
         self.value_board_mat = [[0 for _ in xrange(self.COLS)] for _ in xrange(self.ROWS)]
 
+        # Populate boards with the current matrices
         self._populate_board()
+
+        # Fill the walls in for maze_board
+        self._populate_walls()
 
     def _build_matrix_walls(self, filename):
         '''
@@ -88,32 +96,88 @@ class RootWidgit(FloatLayout):
         return mat_walls, rows, cols
 
     def _populate_board(self):
+        '''
+        This function serves the purpose of populating the GridLayouts set up in
+        the .kv file.
+        - The value_board GridLayout is straight forward since it just takes
+        the values from the matrix and plugs it in.
+        - The maze_board is a bit different because we have to adjust it to
+        add walls in-between
 
+        :return: None
+        '''
+
+        # setting columns for the GridLayout
         self.maze_board.cols = self.BOARD_COLS
         self.value_board.cols = self.COLS
 
         # populate value_board GridLayout
         for x in xrange(self.ROWS):
             for y in xrange(self.COLS):
-                self.value_board.add_widget(Label(text=str(self.value_board_mat[x][y])))
+
+                # Creating buttons for the widgits inside of gridlayouts
+                # because they are more flexible to work with
+                button = Button(text=str(self.value_board_mat[x][y]),
+                                background_disabled_normal='',
+                                disabled_color=[1, 1, 1, 1],
+                                background_normal='',
+                                background_color=[0, 0, 1, 1])
+                
+                # Disable button after creation because background_colors and such
+                # would not save otherwise
+                button.disabled=True
+
+                # Add button to our value_board gridlayout
+                self.value_board.add_widget(button)
+
 
         # populate maze_board GridLayout
         for x in xrange(self.BOARD_ROWS):
 
-            if x % 2 == 0:
+            # Creates the actual vertical path
+            if x % 2 == 1:
                 # Handles horizontal wall widgits as well as the actual path
                 for y in xrange(self.BOARD_COLS):
 
                     # Creates actual path
-                    if y % 2 == 0:
-                        self.maze_board.add_widget(Label(text='a'))
+                    if y % 2 == 1:
+                        self.maze_board.add_widget(Label(text=str(self.maze_board_mat[x/2][y/2])))
                     # Creates wall
                     else:
                         self.maze_board.add_widget(BorderLabel(size_hint=[0.1, 1]))
+            # create the vertical walls
             else:
                 for y in xrange(self.maze_board.cols):
                     self.maze_board.add_widget(BorderLabel(size_hint=[0.1, 0.1]))
 
+    @mainthread
+    def _populate_walls(self):
+
+        label = self.maze_board.children[9]
+
+        print(type(label))
+        label.canvas.before.clear()
+        with label.canvas.before:
+            Color(1, 0, 0, 1)
+            Rectangle(pos=label.pos, size=label.size)
+
+        #with self.canvas.before:
+        #    Color(1, 1, 1, 0.25)
+        #    Rectangle(pos=self.pos, size=self.size)
+
+        print(label.pos)
+        print(label.size)
+
+        for x in xrange(self.ROWS):
+            for y in xrange(self.COLS):
+                if self.mat_walls[x][y][Direction.NORTH.value] == 1:
+                    pass
+                if self.mat_walls[x][y][Direction.EAST.value] == 1:
+                    pass
+                if self.mat_walls[x][y][Direction.SOUTH.value] == 1:
+                    pass
+                if self.mat_walls[x][y][Direction.WEST.value] == 1:
+                    pass
 
 class MazeApp(App):
 
