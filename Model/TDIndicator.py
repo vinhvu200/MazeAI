1a0b5fe67bc9f941a92e7447c261ee83275fc420
vinhvu200 <vinhvu200@gmail.com>
2017-11-20 14:07:27 -0800

    Created a recursive draw method in order to create layered rings to show how sure the AI is about a certain move

--- Model/TDIndicator.py
+++ Model/TDIndicator.py
@@ -1,5 +1,6 @@
 from kivy.uix.widget import Widget
 from kivy.graphics import Color, Rectangle, Ellipse
+from kivy.graphics.instructions import InstructionGroup
 
 
 class TDIndicator(Widget):
@@ -7,7 +8,8 @@ class TDIndicator(Widget):
     def __init__(self, **kwargs):
         super(TDIndicator, self).__init__(**kwargs)
         self.color = [0, 1, 0, 0.65]
-        self.strength = 0
+        self.rgb_color = [0, 1, 0, 0.65]
+        self.strength = 0.0
 
     def draw(self):
         '''
@@ -17,31 +19,12 @@ class TDIndicator(Widget):
         :return:
         '''
 
-        # Get the adjusted distance between parent x,y and child x,y with
-        # the child size be affected by the strength as well
-        adjust_dist_x = self.parent.size[0] - self.size[0] * self.strength
-        adjust_dist_y = self.parent.size[1] - self.size[1] * self.strength
+        self.canvas.clear()
+        start_opacity = 0.2
+        count = 3
 
-        # Divide those distance by two to have the proper distance to adjust
-        adjust_dist_x /= 2
-        adjust_dist_y /= 2
-
-        # Add the adjusted distance to the parent position
-        pos_x = self.parent.pos[0] + adjust_dist_x
-        pos_y = self.parent.pos[1] + adjust_dist_y
-
-        # Store the position as a list
-        pos = [pos_x, pos_y]
-
-        # The indicator size is based on the strength set
-        size_x = self.size[0] * self.strength
-        size_y = self.size[1] * self.strength
-        size = [size_x, size_y]
-
-        # Draw out the appropriate circle for the indicator
-        with self.canvas:
-            Color(0, 1, 0, 0.65)
-            Ellipse(pos=pos, size=size)
+        self._draw(self.parent.size, self.parent.pos, self.strength,
+                   start_opacity, count)
 
     def set_strength(self, highest_val):
         '''
@@ -75,3 +58,41 @@ class TDIndicator(Widget):
             self.strength = 0.9
         else:
             self.strength = 1
+
+    def _draw(self, parent_size, parent_pos, strength, opacity, count):
+
+        if count == 0:
+            return
+
+        if strength < 0:
+            strength = 0
+
+        # Get the adjusted distance between parent x,y and child x,y with
+        # the child size be affected by the strength as well
+        adjust_dist_x = parent_size[0] - self.size[0] * strength
+        adjust_dist_y = parent_size[1] - self.size[1] * strength
+
+        # Divide those distance by two to have the proper distance to adjust
+        adjust_dist_x /= 2
+        adjust_dist_y /= 2
+
+        # Add the adjusted distance to the parent position
+        pos_x = parent_pos[0] + adjust_dist_x
+        pos_y = parent_pos[1] + adjust_dist_y
+
+        # Store position as a list
+        pos = [pos_x, pos_y]
+
+        # The indicator size is based on the strength set
+        size_x = self.size[0] * strength
+        size_y = self.size[1] * strength
+        size = [size_x, size_y]
+
+        # Create the ring Instruction Group
+        ring = InstructionGroup()
+        ring.add(Color(0, 1, 0, opacity))
+        ring.add(Ellipse(pos=pos, size=size))
+        self.canvas.add(ring)
+
+        # Create new values to recurse
+        self._draw(size, pos, strength - 0.2, opacity + 0.2, count - 1)
